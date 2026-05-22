4aa7bad50532a8095a9572b1b0fae95d41ea87ef
Miles Ward <milesward2004@gmail.com>
2026-05-21 14:23:55 -1000

    Migrated to qt6ct theme for qt

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -83,6 +83,9 @@ hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 
+-- QT Config
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+
 -----------------------
 ---- LOOK AND FEEL ----
 -----------------------
