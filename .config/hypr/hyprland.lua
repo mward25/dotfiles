2d97579377831f0c6c96ad8b678b7c6260d6827f
Miles Ward <milesward2004@gmail.com>
2026-05-21 14:24:52 -1000

    Hyprland: Added terminal run command

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -232,6 +232,7 @@ hl.bind(mainMod .. " + CTRL + M", hl.dsp.exit())
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + SHIFT + D", hl.dsp.exec_cmd("rofi -show run -run-command 'kitty -e {cmd}'"))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + X", hl.dsp.layout("togglesplit"))    -- dwindle
 hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen())
